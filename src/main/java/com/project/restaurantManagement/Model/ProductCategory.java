1550c6c34348a81bd76f026fd073d8380f983120
seetal <raayaseetal@gmail.com>
2021-03-22 17:58:52 +0545

    Create Model for ProductCategory

--- src/main/java/com/project/restaurantManagement/Model/ProductCategory.java
+++ src/main/java/com/project/restaurantManagement/Model/ProductCategory.java
@@ -1,5 +1,27 @@
 package com.project.restaurantManagement.Model;
 
-public class ProductCategory {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class ProductCategory {
+    @Id
+    @GeneratedValue
+	 private int pc_id;
+	 private String pc_name;
+	 
+	public int getPc_id() {
+		return pc_id;
+	}
+	public void setPc_id(int pc_id) {
+		this.pc_id = pc_id;
+	}
+	public String getPc_name() {
+		return pc_name;
+	}
+	public void setPc_name(String pc_name) {
+		this.pc_name = pc_name;
+	}
+	 
 }

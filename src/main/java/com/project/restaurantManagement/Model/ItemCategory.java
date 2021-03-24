36f444fe50b095c111b518e67a4fc890e6859faf
seetal <raayaseetal@gmail.com>
2021-03-24 18:50:25 +0545

    Added instances and behaviour

--- src/main/java/com/project/restaurantManagement/Model/ItemCategory.java
+++ src/main/java/com/project/restaurantManagement/Model/ItemCategory.java
@@ -10,7 +10,7 @@ public class ItemCategory {
     @GeneratedValue
 	 private int pc_id;
 	 private String pc_name;
-	 
+	 private String pcimg_location;
 	 
 	public int getPc_id() {
 		return pc_id;
@@ -23,6 +23,16 @@ public class ItemCategory {
 	}
 	public void setPc_name(String pc_name) {
 		this.pc_name = pc_name;
+	}
+	
+	
+	
+	
+	public String getPcimg_location() {
+		return pcimg_location;
+	}
+	public void setPcimg_location(String pcimg_location) {
+		this.pcimg_location = pcimg_location;
 	}
 	@Override
 	public String toString() {

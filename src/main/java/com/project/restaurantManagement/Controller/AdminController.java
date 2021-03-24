84dd307a956a4b99ad7426a8806b21fe2dd1689a
seetal <raayaseetal@gmail.com>
2021-03-24 18:57:06 +0545

    added AdminContoller

--- src/main/java/com/project/restaurantManagement/Controller/AdminController.java
+++ src/main/java/com/project/restaurantManagement/Controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
  
 	@GetMapping("/adminlogin")
-	 public String getAdminPage() {
+	 public String getAdminLoginPage() {
 		 return "adminlogin";
 	 }
 	

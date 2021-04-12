4861e58395a57c0a311657bd86d79283114943a3
Ishwor_007 <raayaseetal@gmail.com>
2021-04-12 16:51:22 +0545

    code updated

--- src/main/java/com/project/restaurantManagement/Controller/AdminController.java
+++ src/main/java/com/project/restaurantManagement/Controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.project.restaurantManagement.Database.AdminDao;
 import com.project.restaurantManagement.Model.Admin;

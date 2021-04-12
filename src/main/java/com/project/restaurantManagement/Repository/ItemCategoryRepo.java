24ddf360dd7e4e0fc7f1b604037e2b2b4bbeb1ac
Ishwor_007 <raayaseetal@gmail.com>
2021-04-12 17:07:50 +0545

    code modified

--- src/main/java/com/project/restaurantManagement/Repository/ItemCategoryRepo.java
+++ src/main/java/com/project/restaurantManagement/Repository/ItemCategoryRepo.java
@@ -2,8 +2,6 @@ package com.project.restaurantManagement.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.project.restaurantManagement.Model.Item;
 import com.project.restaurantManagement.Model.ItemCategory;
 
 @Repository
